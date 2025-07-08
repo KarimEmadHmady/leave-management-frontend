9f466bacb6dcd028a5b3fa05ff7639f0a3d21e22
karim emad <karimkarim20444@gmail.com>
2025-07-08 11:28:24 +0300

    7/8

--- next.config.js
+++ next.config.js
@@ -4,3 +4,15 @@ module.exports = {
     },
   };
   
+
+
+  module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://leave-management-backend-plum.vercel.app/api/:path*',
+      },
+    ];
+  },
+};
